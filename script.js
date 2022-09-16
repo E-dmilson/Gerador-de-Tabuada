d9faa9ea20a4d9e3ab38299e63847c7e44aeb2b4
Edmilson Alves <edmilsonnhamposse@gmail.com>
2022-09-16 09:05:12 +0100

    Ex017 curso em video de Javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+let tb = document.getElementById('tab').addEventListener('click', tabuada)
+
+function tabuada(){
+    let num = document.getElementById('num')
+    let tab = document.getElementById('seltab')
+
+    if(num.value.length == 0){
+        alert('por favor digite um erro')
+    }else{
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while(c <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
